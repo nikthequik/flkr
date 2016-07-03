f15c1fe1e17331fb8f465e22bbcbecde72d91ad6
nikthequik <quik.the.nick@gmail.com>
2016-07-03 00:43:52 -0600

    -can now display images on page

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
-var app = angular.module("flkr", []);
+var app = angular.module("flkr", ['ngAnimate']);
 
-app.controller("mainCtrl", function($http, $scope, $q){
+app.controller("mainCtrl", function($http, $scope, $q, $timeout){
 	var vm = this;
-	vm.searchTag = "";
 	vm.exeSearch = function(){
+		vm.notify = true;
+		vm.currentTag = vm.searchTag;
+		getQuery();
+	};
+	function getQuery() {
 		var url = "https://api.flickr.com/services/rest";
 		var params = {
 		    method: 'flickr.photos.search',
@@ -12,13 +16,31 @@ app.controller("mainCtrl", function($http, $scope, $q){
 		    format: 'json',
 		    nojsoncallback: 1
 		};
-		$http.get(url + "?method=flickr.photos.search&api_key=eca4a39f4530784cab19d49b88b65d45&tags=" + vm.searchTag +
-			"&format=json&nojsoncallback=1"
-		)
+		$http({
+			url: url,
+			method: 'GET',
+			params: params
+		})
 		.then(function(res){
-			console.log(res);
+			vm.results = res.data.photos.photo;
+			vm.success = true;
+			console.log(vm.results);
+		}, function(res){
+			console.log(res.error);
 		});
-/*		var oauth = new OAuth.OAuth(
+	};
+
+
+	vm.results = [];
+	vm.searchTag = "";
+	vm.notify = false;
+	vm.success = false;
+		
+});
+/*getRecentEndpoint  https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=74585bd94ec3f63f8ce89fc71b7e6a66&format=json&nojsoncallback=1&auth_token=72157670536367065-969c56630cf9e302&api_sig=e0e658b58b7e98130496c7c4dbeb8275*/
+
+/*		oAuth Setup
+		var oauth = new OAuth.OAuth(
 	      'https://www.flickr.com/services/oauth/request_token',
 	      'https://www.flickr.com/services/oauth/access_token',
 	      'eca4a39f4530784cab19d49b88b65d45',
@@ -34,7 +56,3 @@ app.controller("mainCtrl", function($http, $scope, $q){
 	      function (data){      
 	        console.log(data);   
 	    });   */
-	};
-		
-});
-/*getRecentEndpoint  https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=74585bd94ec3f63f8ce89fc71b7e6a66&format=json&nojsoncallback=1&auth_token=72157670536367065-969c56630cf9e302&api_sig=e0e658b58b7e98130496c7c4dbeb8275*/
